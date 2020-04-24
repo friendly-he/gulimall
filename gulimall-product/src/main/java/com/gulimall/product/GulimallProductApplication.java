869fd9ca833f53a25e721666f3fd615d55972276
hl <1182922535@qq.com>
2020-04-24 09:46:00 +0800

    gulimall spring cloud

--- gulimall-product/src/main/java/com/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/gulimall/product/GulimallProductApplication.java
@@ -1,9 +1,16 @@
 package com.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@MapperScan("com.gulimall.product.dao")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.gulimall.product.openfeign")
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
--- gulimall-product/src/test/java/com/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,32 @@
 package com.gulimall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.gulimall.product.entity.BrandEntity;
+import com.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
-
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("小米");
+//        brandEntity.setDescript("12131");
+//        brandService.save(brandEntity);
+//        BrandEntity byId = brandService.getById(1);
+//        System.out.println(byId);
+        BrandEntity one = brandService.getOne(new QueryWrapper<BrandEntity>().eq("name", "小米"));
+        System.out.println(one);
+
     }
+
 
 }
